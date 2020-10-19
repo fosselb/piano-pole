831faf73345d12b4d49e67eeb27f48f2dff027a7
Evan Mitchell <evan.m.mitchell@gmail.com>
2020-10-18 17:57:59 -0700

    Collect MPU-6050 measurements and write to file

---
+++ serial_log.py
@@ -0,0 +1,12 @@
+import serial
+
+port = "COM6"           # Windows
+# port = "/dev/ttyUSB0"   # macOS
+
+with open("log.csv", "wb") as file:
+    ser = serial.Serial(port, 9600, timeout=10)
+
+    # while True:             # Record until stopped (Ctrl+C)
+    for _ in range(500):    # Record n lines
+        line = ser.readline()
+        file.write(line)
