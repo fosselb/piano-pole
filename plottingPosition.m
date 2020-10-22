3b2d393aba4a51ed1f9fc6774cfd4dfad77fa496
fosselb <flinbian@lion.lmu.edu>
2020-10-22 15:00:51 -0700

    minor edits

--- plottingPosition.m
+++ plottingPosition.m
@@ -1,12 +1,9 @@
-%Testing GY-521 Module to determine position
+%Testing MPU-6050 module to determine position
 
 clc;
 close all;
 clear;
 format compact;
-
-% filename = 'log.csv';
-% M = csvread(filename);
 
 T = readtable('log.csv');
 
@@ -14,11 +11,12 @@ ax = table2array(T(:, 2));
 ay = table2array(T(:, 3));
 az = table2array(T(:, 4));
 
+% use cumtrapz() for integration
 
-% cumtrapz
-
+% plot data
 figure;
 plot3(ax, ay, az, 'o');
-xlabel("X-axis");
-ylabel("Y-axis");
-zlabel("Z-axis");
+title("Acceleration Data from MPU-6050 Module");
+xlabel("Accel X (m/s^2)");
+ylabel("Accel Y (m/s^2)");
+zlabel("Accel Z (m/s^2)");
