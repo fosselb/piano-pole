4592451068b961c7d4b7f70e00528d20a21b2d40
fosselb <flinbian@lion.lmu.edu>
2021-02-21 08:52:00 -0800

    attempted to read RFID module with MicroPython

--- XBee_Thing_Plus_Module/main.py
+++ XBee_Thing_Plus_Module/main.py
@@ -2,6 +2,7 @@ import machine
 import sys
 import utime
 import xbee
+from machine import I2C
 
 # Pin definitions
 repl_button = machine.Pin(machine.Pin.board.D5, machine.Pin.IN, machine.Pin.PULL_UP)
@@ -10,8 +11,15 @@ led = machine.Pin(machine.Pin.board.D4, machine.Pin.OUT)
 x = xbee.XBee()
 test_data = 'Hello World!'
 
+i2c = I2C(1, freq=400000)
+
 # Wait for button 5 to be pressed, and then exit
 while True:
+
+    i2c.scan()
+    #scanned_address = i2c.scan()
+    #print("Scanned Address: %s" % scanned_address)
+
     # if button 5 is pressed, drop to repl_button
     if  repl_button.value() == 0:
         #print("Dropping to REPL")
@@ -21,7 +29,6 @@ while True:
         xbee.transmit(xbee.ADDR_COORDINATOR, test_data)
 
         #sys.exit()
-
 
     # turn LED on and off
     led.value(1)
