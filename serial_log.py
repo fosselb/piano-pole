91c14227b987bf799daa606a806e35fe906f6aa8
Evan Mitchell <evan.m.mitchell@gmail.com>
2021-03-09 09:56:41 -0800

    Fix style

--- serial_log.py
+++ serial_log.py
@@ -1,17 +1,19 @@
 import serial
-from time import sleep
 
-# port = "COM6"           # Windows
-port = "/dev/cu.usbmodem145401"   # macOS
+port = "COM6"   # Windows
+# port = "/dev/cu.usbmodem145401"     # macOS
 
-with open("log.csv", "wb") as file:
+with open("logBNO085-stability.csv", "wb") as file:
     ser = serial.Serial(port, 115200, timeout=10)
 
-    for _ in range(1000):
+    for _ in range(100):
         line = ser.readline()
 
-    # while True:             # Record until stopped (Ctrl+C)
-    for _ in range(5000):    # Record n lines
-        print("...")
+    i = 0
+    while True:         # Record until stopped (Ctrl+C)
+    # while i < 10000:    # Record n lines
+        if (i % 500 == 0):
+            print(".")
         line = ser.readline()
         file.write(line)
+        i += 1
