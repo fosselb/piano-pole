f89d1f32c084e7e2071349212f1a8bab517a2332
Evan Mitchell <evan.m.mitchell@gmail.com>
2021-03-29 01:45:56 -0700

    Decode XBee message

--- XBee_Coordinator/XBee_Coordinator.ino
+++ XBee_Coordinator/XBee_Coordinator.ino
@@ -6,13 +6,14 @@
 
 #include <SoftwareSerial.h>
 
-char startByte = 0x7E;
-int dataCount = 0;
-byte analog_MSB;
-byte analog_LSB;
-int analog_reading_Z;
-int analog_reading_Y;
-int analog_reading_X;
+const char START_BYTE = 0x7E;
+byte current;
+byte previous;
+int byteNum = -1;
+int length = -1;
+int address;
+byte checksum;  //Only care about least significant byte, so  doesn't matter if carry is dropped
+String line;
 
 //SparkFun RedBoard
 SoftwareSerial XBee(2, 3); // XBee DOUT, IN - Arduino pin 2, 3 (RX, TX)
@@ -23,52 +24,47 @@ SoftwareSerial XBee(2, 3); // XBee DOUT, IN - Arduino pin 2, 3 (RX, TX)
 void setup() {
   XBee.begin(115200);
   Serial.begin(115200);
-  Serial.println("Reading XBee Data");
 }
 
 void loop() {
   if (XBee.available()) {
-    char current = XBee.read();
-    if (current == 0x7E) {
+    current = XBee.read();
+    if (current == START_BYTE) {
       Serial.println();
-      Serial.println();
-      Serial.println("Start:");
-      dataCount = 0;
+      line = "";
+      byteNum = 0;
+      length = -1;
+      checksum = 0;
+    } else if (byteNum == -1) {
+      return;
     }
 
-    dataCount++;
+    // Serial.print(current, HEX);
+    // Serial.print(",");
 
-// * to view entire package of bytes coming in *
-    Serial.print(current, HEX);
-    Serial.print(",");
-
-// * to view parsed out analog data *
-    if (dataCount == 12) {
-      analog_MSB = current;
-    } else if (dataCount == 13) {
-      analog_LSB = current;
-      analog_reading_Z = (analog_MSB * 256) + analog_LSB;
-      analog_reading_Z -= Z_accel_at_rest;
-      Serial.print(analog_reading_Z);
-      Serial.print("\t");
-    } else if (dataCount == 14) {
-      analog_MSB = current;
-    } else if (dataCount == 15) {
-      analog_LSB = current;
-      analog_reading_Y = (analog_MSB * 256) + analog_LSB;
-      analog_reading_Y -= Y_accel_at_rest;
-      Serial.print(analog_reading_Y);
-      Serial.print("\t");
-    } else if (dataCount == 16) {
-      analog_MSB = current;
-    } else if (dataCount == 17) {
-      analog_LSB = current;
-      analog_reading_X = (analog_MSB * 256) + analog_LSB;
-      analog_reading_X -= X_accel_at_rest;
-      Serial.print(analog_reading_X);
-      Serial.print("\n");
+    if (byteNum == 2) {
+      length = (previous << 8) | current;
+      length += 3;  //Include start and length bytes but NOT checksum byte
+    } else if (byteNum == 5) {
+      address = (previous << 8) | current;
+      Serial.println(address);  //Print source address
+    } else if (byteNum >= 8 && byteNum < length) {
+      line += (char) current;  //Append data
     }
-//  } else {
-//    //sendRandomData();
+
+    if (byteNum > 2) {
+      checksum += current;
+    }
+
+    if (byteNum == length) {
+      if (checksum == 0xFF) {
+        Serial.print(line);
+      } else {
+        Serial.println("<ERROR>");
+      }
+    }
+
+    previous = current;
+    byteNum++;
   }
 }
