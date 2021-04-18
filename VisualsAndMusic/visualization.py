91c41d03bd946b8c3a934374259bcf2a5386af89
fosselb <flinbian@lion.lmu.edu>
2021-04-18 09:04:48 -0700

    adding visuals and music

---
+++ VisualsAndMusic/visualization.py
@@ -0,0 +1,78 @@
+from processing_py import *
+import csv
+import sounddevice as sd
+import soundfile as sf
+
+filename = 'A3vH.wav'
+# Extract data and sampling rate from file
+data, fs = sf.read(filename, dtype='float32')
+
+height = 800
+width = 800
+circle_diameter = 10;
+t = []
+x1 = []
+y1 = []
+x2 = []
+y2 = []
+x3 = []
+y3 = []
+x4 = []
+y4 = []
+i = 0
+
+app = App(width,height) # create window: width, height
+
+with open("rose_data.csv") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for lines in csv_reader:
+      t.append(lines[0])
+      x1.append(lines[1])
+      y1.append(lines[2])
+      x2.append(lines[3])
+      y2.append(lines[4])
+      x3.append(lines[5])
+      y3.append(lines[6])
+      x4.append(lines[7])
+      y4.append(lines[8])
+
+t = [int(i) for i in t]
+x1 = [int(i) for i in x1]
+y1 = [int(i) for i in y1]
+x2 = [int(i) for i in x2]
+y2 = [int(i) for i in y2]
+x3 = [int(i) for i in x3]
+y3 = [int(i) for i in y3]
+x4 = [int(i) for i in x4]
+y4 = [int(i) for i in y4]
+
+app.background(0) # set background
+app.fill(255) # set white circle to represent pole
+app.ellipse(width/2, height/2, 40, 40)
+
+
+while True:
+
+    app.fill(0, 255, 0)
+    app.ellipse(x1[i] + width/2, y1[i] + height/2, circle_diameter, circle_diameter)
+    app.fill(255, 0, 0)
+    app.ellipse(x2[i] + width/2, y2[i] + height/2, circle_diameter, circle_diameter)
+    app.fill(0, 0, 255)
+    app.ellipse(x3[i] + width/2, y3[i] + height/2, circle_diameter, circle_diameter)
+    app.fill(255, 255, 0)
+    app.ellipse(x4[i] + width/2, y4[i] + height/2, circle_diameter, circle_diameter)
+
+    if i < len(t) - 1:
+        i = i + 1
+    else:
+        i = 0
+
+    sd.play(data, fs)
+    app.redraw() # refresh the window
+
+
+    #status = sd.wait()  # Wait until file is done playing
+
+
+
+#app.exit() # close the window
